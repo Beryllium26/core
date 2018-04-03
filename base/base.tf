2fa1854bd7574f55e0bcd13cb6d34de14d268317
Laurent Allegre <laurent@laurents-mbp.home>
2018-04-03 23:26:52 +0100

    tags need to be specified in resources: tags = "${merge(var.default_tags, map

--- base/base.tf
+++ base/base.tf
@@ -8,6 +8,7 @@ provider "aws" {
 resource "aws_instance" "base1" {
 	ami	= "ami-dff017b8"
 	instance_type = "t2.micro"
+	tags = "${merge(var.default_tags, map("Name", "base1"))}"
 }
 
 resource "aws_eip" "ip" {
