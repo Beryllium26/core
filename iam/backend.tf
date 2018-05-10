7f743d94edd3ac99cfd6685fc159690c6ce1d478
Laurent Allegre <laurent@pecaire.local>
2018-05-10 11:50:28 +0100

    using defined ${var.environment} in variables.tf

--- iam/backend.tf
+++ iam/backend.tf
@@ -2,7 +2,7 @@
 terraform {
     backend "s3" {
         bucket          = "orgfxe-1a-core-infra-tfstate"
-        key             = "iam/terraform.tfstate"
+        key             = "${var.environment}/terraform.tfstate"
         region          = "eu-west-2"
         profile         = "core-infra"
     }
